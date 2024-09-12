45c69de943a33929ec2897c4d04e8aad22cd3820
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-09-12 02:55:17 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Screen Match");
+        System.out.println("Pelicula: Matrix");
+
+        int fechaDeEstreno = 1999;
+        boolean includioEnElPlan = true;
+        double calificacion = 8.2;
+
+        double media = (8.2 + 9.5 + 8.0) / 3;
+        System.out.println("La media de las calificaciones es: " + media);
+    }
+}
